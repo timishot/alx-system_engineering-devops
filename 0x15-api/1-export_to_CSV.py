f9827d1916de104e73a9d2d341d364bed9235b1f
Timishot <timidotcom58@gmail.com>
2024-01-02 15:17:30 +0100

    Fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Write a Python script that, using this REST API, for a given employee ID,
  returns information about his/her TODO list progress."""
+import csv
 import requests
 import sys
-import csv
+
 
 
 def export_todo_list_to_csv(employee_id):
