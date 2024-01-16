cba37ec57b0c8bb21c99eb2c6b5fd053cc9846a6
Timishot <timidotcom58@gmail.com>
2024-01-16 09:17:42 +0100

    unction that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""unction that queries the Reddit API and returns the number of subscribers"""
+import requests
+
+def number_of_subscribers(subreddit):
+	url = f'https://www.reddit.com/r/{subreddit}/about.json'
+	headers = {'User-Agent': 'Custom User Agent'}
+
+	response = requests.get(url, headers=headers)
+
+	"""Check if request is successful"""
+	if response.status_code == 200:
+		try:
+			data = response.json()
+			subscribers_count = data['data']['subscribers']
+			return subscribers_count
+		except KeyError:
+			return 0
+	else:
+		return 0
