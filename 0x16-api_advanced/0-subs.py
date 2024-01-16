77885e5e231bff71426590af073db104a6afb07b
Timishot <timidotcom58@gmail.com>
2024-01-16 10:15:55 +0100

    rite a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,19 +2,20 @@
 """unction that queries the Reddit API and returns the number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-	url = f'https://www.reddit.com/r/{subreddit}/about.json'
-	headers = {'User-Agent': 'Custom User Agent'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'Custom User Agent'}
 
-	response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-	"""Check if request is successful"""
-	if response.status_code == 200:
-		try:
-			data = response.json()
-			subscribers_count = data['data']['subscribers']
-			return subscribers_count
-		except KeyError:
-			return 0
-	else:
-		return 0
+    """Check if request is successful"""
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            subscribers_count = data['data']['subscribers']
+            return subscribers_count
+        except KeyError:
+            return 0
+    else:
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""unction that queries the Reddit API and returns the number of subscribers"""
+import requests
+
+
+def top_ten(subreddit):
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'Custom User Agent'}
+    params = {"limit": 10}
+
+    response = requests.get(url, headers=headers, params=params)
+
+    """Check if request is successful"""
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                title = post['data']['title']
+                print(title)
+        except KeyError:
+            return None
+    else:
+        return None
