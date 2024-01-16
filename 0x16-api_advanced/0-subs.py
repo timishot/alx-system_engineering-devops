3120cb84627a46b08b2a318accf2153c8b5abef2
Timishot <timidotcom58@gmail.com>
2024-01-16 12:14:25 +0100

    Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""unction that queries the Reddit API and returns the number of subscribers"""
+"""Function that queries the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'Custom User Agent'}
 
