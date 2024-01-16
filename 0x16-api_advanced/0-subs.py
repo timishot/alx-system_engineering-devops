69ab785fd434ba9c2bf11afec1c5c5c6d64a9d47
Timishot <timidotcom58@gmail.com>
2024-01-16 12:15:33 +0100

    Fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Function that queries the Reddit API and returns the number of subscribers"""
+"""Function that queries the Reddit API and
+ returns the number of subscribers"""
 import requests
 
 
